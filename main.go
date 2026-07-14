068531dff832954725013ddb5163353e6a41fc43
FishFuel <fishfuel@pm.me>
2026-07-14 16:05:19 -0500

    Convert tasks to structs

--- main.go
+++ main.go
@@ -7,8 +7,14 @@ import (
 	"strings"
 )
 
+type Task struct {
+	Done  bool
+	Title string
+}
+
 func main() {
-	var tasks []string
+
+	var tasks []Task
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for {
@@ -22,15 +28,20 @@ func main() {
 			return
 
 		case cmd == "list":
-			for i, task := range tasks {
-				fmt.Printf("%d. %s\n", i+1, task)
+			for i, t := range tasks {
+				check := "[ ]"
+				if t.Done {
+					check = "[x]"
+				}
+				fmt.Printf("%d. %s %s\n", i+1, check, t.Title)
 			}
 
 		// the rest of the input after "add " is the task
 		case strings.HasPrefix(cmd, "add "):
-			task := input[4:] // slice off "add " (4 characters)
-			tasks = append(tasks, task)
-			fmt.Println("Added:", task)
+			title := input[4:] // slice off "add " (4 characters)
+			newTask := Task{Title: title, Done: false}
+			tasks = append(tasks, newTask)
+			fmt.Println("Added:", title)
 
 		default:
 			fmt.Println("Unknown command:", input)
